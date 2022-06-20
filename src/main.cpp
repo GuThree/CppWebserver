3d2782bc9a05a0b2af63796b9b7d0c9a59405ab3
gsr <695579980@qq.com>
2022-06-20 19:58:05 +0800

    小改

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
 {
     signal(SIGINT, signal_handler);
     signal(SIGKILL, signal_handler);
+    signal(SIGTERM, signal_handler);
     HttpServer httpServer(port);
     httpServer.run(8, 5000);
     return 0;
